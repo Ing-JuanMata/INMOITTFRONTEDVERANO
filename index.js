ee62bf25707f915ea658f609e0bc34baa2a4889b
Juan Jesus Mata Solis <jujemataso@ittepic.edu.mx>
2022-09-11 12:09:37 -0600

    otros ajustes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
-const session = require("express-session");
+// const session = require("express-session");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const multer = require("multer");
+// const multer = require("multer");
 const routes = require("./routes");
 //const DataRoutes = require("./api/routes");
 
@@ -26,22 +26,22 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 //middlewares
-app.use(multer({ storage }).single("img"));
+//app.use(multer({ storage }).single("img"));
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  session({
-    saveUninitialized: true,
-    resave: true,
-    secret: "InmoITT",
-  })
-);
+// app.use(
+//   session({
+//     saveUninitialized: true,
+//     resave: true,
+//     secret: "InmoITT",
+//   })
+// );
 
 // routes
 app.use(routes);
-app.use("/data", DataRoutes);
+//app.use("/data", DataRoutes);
 
 // static files
 // app.use(express.static(path.join(__dirname, "public")));
